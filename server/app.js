a25003dbf127f98d175e43b5ea6b43d6a87b4900
PrakruthiMadhav <prakruthimadhav@gmail.com>
2024-04-10 08:29:15 +0530

    different changes

--- server/app.js
+++ server/app.js
@@ -2,13 +2,20 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const { sendEmail } = require('./mail');
+const { sendEmail, sendOtpVerificationEmail } = require('./mail');
 const userModel = require('./models/users');
+const otpModel = require('./models/otp');
+const otp = require('./mail');
 const axios = require('axios');
 const cors = require('cors');
+const bcrypt = require('bcrypt');
+
 mongoose.connect("mongodb+srv://ompatil2002:chocolate19@cluster0.n0qwari.mongodb.net/faceauth")
+
 app.use(express.json());
 app.use(cors());
+
+
 //Route is used to get all users form databse
 app.get('/getUsers', async (req, res) => {
   try {
@@ -18,13 +25,27 @@ app.get('/getUsers', async (req, res) => {
     res.json(err);
   }
 });
-//Create a new user inside the database
-app.post("/createUser", async (req, res) => {
-  const user = req.body;
-  const newUser = new userModel(user);
-  await newUser.save();
 
-  res.json(user);
+app.post("/createUser", async (req, res) => {
+  try {
+    let pass = "";
+    if (req.body.password === "") {
+      pass = "";
+    }
+    else {
+      pass = await bcrypt.hash(req.body.password, 10);
+    }
+    const newUser = new userModel({
+      name: req.body.name,
+      password: pass,
+      email: req.body.email
+    });
+    await newUser.save();
+    res.send(console.log("User finally saved"));
+    await otpModel.deleteMany({ otp: otp.otp });
+  } catch (err) {
+    console.log("error while saving your acc because of incorrect email", err);
+  }
 });
 //to check if the user exists in the database
 app.get(`/getUsers/:email`, async (req, res) => {
@@ -60,6 +81,51 @@ app.get(`/database`, async (req, res) => {
   });
   res.send(console.log("GG added to database successfully"))
 })
+
+
+//sending otp through email
+app.post("/sendveriemail/:email", async (req, res) => {
+  //handle acc verification
+  const email = req.params.email;
+  await sendOtpVerificationEmail(email, res);
+});
+
+app.post("/verifyOTP/:otp", async (req, res) => {
+  const otp_entered = parseInt(req.params.otp);
+  const email = req.query.email;
+  console.log("email in verifyOTP", email);
+  console.log("frontend otp", otp_entered);
+  console.log("backend otp", otp.otp);
+  console.log("type of frontend otp", typeof (otp_entered));
+  console.log("type of backend otp", typeof (otp.otp));
+  try {
+
+    const otpVerification = await otpModel.findOne({ otp: otp_entered });
+    console.log("otpVerification", otpVerification);
+
+    if (otpVerification === null) {
+      //if it's null or incorrect
+      res.json({ status: 'failure', message: 'OTP incorrect.Enter correct one' });
+    }
+    else {
+      // use the otp variable which holds the OTP data
+      if (otp_entered === otp.otp) {
+        console.log("Otp verified successfully")
+        res.json({ status: 'success', message: 'OTP verified successfullyyyyyyyyy' });
+      }
+
+      else if (otpVerification.expiresAt < Date.now()) {
+        res.json({ status: 'failure', message: 'OTP expired' });
+        await sendOtpVerificationEmail(email, res);
+      }
+    }
+    console.log("otp given", otpVerification.expiresAt);
+    console.log("type of otp given", typeof (otpVerification.expiresAt));
+
+  } catch (error) {
+    res.send(console.log("Internal server error",error))
+  }
+});
 
 //listening to the server side port
 app.listen(1900, () => {
--- server/mail.js
+++ server/mail.js
@@ -1,9 +1,19 @@
 const nodemailer=require('nodemailer');
+// const { google } = require('googleapis');
+const otpModel = require('./models/otp');
 
 const CLIENT_ID="your client id"
-
 const CLIENT_SECRET="Your Client Secret"
+const REDIRECT_URL = "https://developers.google.com/oauthplayground"
+const REFRESH_TOKEN = "Enter your Refresh Token here";
 
+// const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL)
+// oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN })
+let otp = Math.floor(100000 + Math.random() * 900000);
+
+//const accessToken =  oAuth2Client.getAccessToken();
+const accessToken="Enter your accessToken here";
+ 
 async function sendEmail(e,time){
     try{
         const transport=nodemailer.createTransport({
@@ -13,6 +23,7 @@ async function sendEmail(e,time){
                 user:"sender email",
                 clientid: CLIENT_ID,
                 clientsecret: CLIENT_SECRET,
+                refreshToken: REFRESH_TOKEN,
                 //Have to keep updating access token every one hour
                 accessToken:"your access token",
             }
@@ -24,11 +35,49 @@ async function sendEmail(e,time){
             text: "hello",
             html:`<h4> Hello! ${time.name} you have authenticated successfully at ${time.timestamp}</h4>`,
         }
-        const result=await transport.sendMail(mailOptions);
+        await transport.sendMail(mailOptions);
 
-        return result;
+        res.send(console.log("HURRAY email sent successfully"))
     }catch(error){
         return error;
     }
 }
-module.exports = { sendEmail };
+async function sendOtpVerificationEmail(email, res) {
+    try {
+        
+        let transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                type: "OAuth2",
+                user: process.env.USER,
+                clientId: CLIENT_ID,
+                clientSecret: CLIENT_SECRET,
+                refreshToken: REFRESH_TOKEN,
+                accessToken: accessToken
+            }
+        });
+        const newOtpVerification = new otpModel({
+            
+            otp:otp,
+            createdAt: Date.now(),
+            expiresAt: Date.now() + 900000,
+        });
+        await newOtpVerification.save();
+
+        console.log("EMAIL", email);
+        const options = {
+            from: process.env.USER,
+            to: email,
+            subject: "Verify your Email",
+            html: `<h1> Your FaceAuth Member Profile code</h1> <br> <h4> Here's the one-time verification code you requested: <h2> ${otp} </h2>This code expires after 15 minutes. If you've already received this code or don't need it any more, ignore this email. </h4>`
+        }
+        await transporter.sendMail(options).catch(err => console.log(err));       
+        res.send(console.log("HURRAY otp mail sent successfully"))
+    } catch (error) {
+      
+        // res.send(console.log("BOOO otp mail sent failed"))
+        res.send(console.log("error while sending otp is", error));
+    }
+}
+
+module.exports = { sendEmail, sendOtpVerificationEmail, otp };
